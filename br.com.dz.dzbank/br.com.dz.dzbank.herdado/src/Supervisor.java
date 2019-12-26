dfc50e76a8352199f5ab02d9d0300d1422a10aa2
danzanatta <50952439+danzanatta@users.noreply.github.com>
2019-12-26 00:03:50 -0200

    Atualização

--- br.com.dz.dzbank/br.com.dz.dzbank.herdado/src/Supervisor.java
+++ br.com.dz.dzbank/br.com.dz.dzbank.herdado/src/Supervisor.java
@@ -1,12 +1,12 @@
-// Diretor é um funcionário, Diretor herda da class Funcionario, assina o contrato Autenticavel
-public class Diretor extends Funcionario implements Autenticavel {
+// Supervisor é um funcionário, Supervisor herda da class Funcionario, assina o contrato Autenticavel
+public class Supervisor extends Funcionario implements Autenticavel {
 
 	private int senha; 
 	
 	@Override
 	public double getBonificacao() {
 		System.out.println("Método de Bonificação do Supervisor");
-		return 150;
+		return 100;
 	}
 	
 	@Override
--- br.com.dz.dzbank/br.com.dz.dzbank.herdado/src/TesteSistema.java
+++ br.com.dz.dzbank/br.com.dz.dzbank.herdado/src/TesteSistema.java
@@ -14,11 +14,15 @@ public class TesteSistema {
 		Cliente cli = new Cliente();
 		cli.setSenha(2222);
 		
+		Supervisor sup = new Supervisor();
+		sup.setSenha(2222);
+		
 		SistemaInterno si = new SistemaInterno();
 		si.autentica(g);
 		si.autentica(adm);
 		si.autentica(dir);
 		si.autentica(cli);
+		si.autentica(sup);
 	}
 
 }
