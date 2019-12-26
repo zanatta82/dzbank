1d55626792ec4f75a78a012445e3c35a6483694b
danzanatta <50952439+danzanatta@users.noreply.github.com>
2019-12-26 16:16:42 -0200

    Entendento Exceções

--- br.com.dz.dzbank/br.com.dz.java.pilha/src/Fluxo.java
+++ br.com.dz.dzbank/br.com.dz.java.pilha/src/Fluxo.java
@@ -1,24 +1,38 @@
-
 public class Fluxo {
 
 	public static void main(String[] args) {
-		System.out.println("Inicio do main");
-		metodo1();
-		System.out.println("Fim do main");
+		System.out.println("Início do Main");
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception " + msg);
+			ex.printStackTrace();
+		}
+		System.out.println("Fim do Main");
 	}
 
 	public static void metodo1() {
-		System.out.println("Inicio do metodo1");
+		System.out.println("Início do Método 1");
 		metodo2();
-		System.out.println("Fim do metodo1");
+		System.out.println("Fim do Método 1");
 	}
 
 	private static void metodo2() {
-		System.out.println("Inicio metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-		}
-		System.out.println("Fim do metodo2");
+		System.out.println("Início do Método 2");
+		metodo3();
+		System.out.println("Fim do Método 2");
 	}
 
+	private static void metodo3() {
+		System.out.println("Início do Método 3");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+
+			//Conta c = null;
+			//c.deposita();
+		}
+		System.out.println("Fim do Método 3");
+	}
 }
